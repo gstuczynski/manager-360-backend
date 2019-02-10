89bb4bbcf44d9198767f2cbf87d1fa467fd4c60c
Grzegorz Stuczynski <stuczynski.g@gmail.com>
2019-02-10 21:19:12 +0100

    Added get returning all files

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const multer  = require('multer')
 const bodyParser = require("body-parser");
+const fs = require('fs');
+
+const imagesDir = '../images360'
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, '../uploads'),
+  destination: (req, file, cb) => cb(null, imagesDir),
   filename: (req, file, cb) =>  cb(null, file.originalname)
 })
 
@@ -12,6 +15,16 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/all-images', (req, res) => {
+  fs.readdir(imagesDir, (err, files) => {
+    if(err){
+      res.status(500).send("Something went wrong")
+    }else {
+      res.sendStatus(200)
+    }
+  })
+})
 
 app.post('/upload',(req, res) => {
   upload(req, res, (err) => {
